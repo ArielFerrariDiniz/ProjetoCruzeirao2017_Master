7c1232bb7ebcbd7b89f608934d0aff90b841cd6e
Ariel <Ariel@Ariel-PC1>
2017-05-04 13:19:54 -0300

    Master

---
+++ ProjetoModeloCruzeirao/src/main/java/sistema/modelos/EstatisticaPartida.java
@@ -0,0 +1,25 @@
+package sistema.modelos;
+
+public class EstatisticaPartida {
+	private String resultado;
+	private int cartoes;
+	private int gols;
+	public String getResultado() {
+		return resultado;
+	}
+	public void setResultado(String resultado) {
+		this.resultado = resultado;
+	}
+	public int getCartoes() {
+		return cartoes;
+	}
+	public void setCartoes(int cartoes) {
+		this.cartoes = cartoes;
+	}
+	public int getGols() {
+		return gols;
+	}
+	public void setGols(int gols) {
+		this.gols = gols;
+	}
+}
